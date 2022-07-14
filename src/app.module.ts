138fb268c1db719d92be24e41a74ffa31c584423
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-07-14 14:36:06 -0300

    add config database and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
